97141f5c83b42ab872abc746e0d7f6951b9dbe0a
mohsinht <23508720+mohsinht@users.noreply.github.com>
2019-09-04 01:30:52 +0500

    file read

--- part_1.py
+++ part_1.py
@@ -1,1 +1,15 @@
-print("github testing")
+import config
+from bs4 import BeautifulSoup
+from os import listdir
+from os.path import isfile, join
+
+
+def extractTextFromHtml(dir):
+    #read file names
+    onlyfiles = [f for f in listdir(dir) if isfile(join(dir, f))]
+    print(onlyfiles)
+
+
+print("getting files from " + config.DIR)
+extractTextFromHtml(config.DIR)
+
