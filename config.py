3b7b5b38bb07db7eb106c1a12338c87d8324ab3a
mohsinht <23508720+mohsinht@users.noreply.github.com>
2019-09-04 02:10:40 +0500

    tokenize done

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
-DIR = '../corpus/'
+CORPUS_DIR = '../corpus/'
+STOPLIST_FILE = '../stoplist.txt'
--- part_1.py
+++ part_1.py
@@ -2,6 +2,7 @@ import config
 from bs4 import BeautifulSoup
 from os import listdir
 from os.path import isfile, join
+import re
 
 def parseHtml(file_html):
     file_html = '<!DOCTYPE html' + file_html.split('<!DOCTYPE html', 1)[1]  # skipping all info before html starts
@@ -16,19 +17,27 @@ def parseHtml(file_html):
     file_text = '\n'.join(chunk for chunk in chunks if chunk)
     return file_text
 
+def tokenize(text):
+    file_text = text.lower()
+    return re.split("[ .,!?:;'\n\"\-]+", file_text)
+
 def processFiles(dir):
     #read file names
     file_names = [dir+file for file in listdir(dir) if isfile(join(dir, file))]
+    print("Total Files: " + str(len(file_names)))
     for file_name in file_names:
         file_pointer = open(file_name, "r")
         file_html = file_pointer.read()
         parseHtml(file_html)
         file_text = parseHtml(file_html)
-        print(file_text)
+
+        tokens = tokenize(file_text)
+
+        print(tokens)
         file_pointer.close()
         break
 
 
-print("getting files from " + config.DIR)
-processFiles(config.DIR)
+print("getting files from " + config.CORPUS_DIR)
+processFiles(config.CORPUS_DIR)
 
