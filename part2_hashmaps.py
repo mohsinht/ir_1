be0173d1ef657a934be8ee87e848c5ae0df821d0
mohsinht <23508720+mohsinht@users.noreply.github.com>
2019-09-15 01:52:38 +0500

    saving stats of terms in part 2 (simple)

--- part2_hashmaps.py
+++ part2_hashmaps.py
@@ -3,53 +3,16 @@ from bs4 import BeautifulSoup
 import re
 from nltk.stem import PorterStemmer
 
-class Doc:
-    id = 0
-    value = "null"
-    def __init__(self, id, value):
-        self.id = id
-        self.value = value
-
-
-class Term:
-    id = 0
-    value = "null"
-    docids = []
-    positions = []
-    totalCount = 0
-    docFreq = 0
-
-    def __init__(self, id, value):
-        self.id = id
-        self.value = value
-        self.docids = []
-        self.positions = []
-
-    def add(self, docid, position):
-        if docid not in self.docids:
-            self.docFreq += 1
-        self.docids.append(docid)
-        self.positions.append(position)
-        self.totalCount += 1
-
-    def getStats(self):
-        stats = str(self.id) + " " + str(self.totalCount) + " " + str(self.docFreq)
-        i = 0
-        for docId in self.docids:
-            stats += " " + str(docId) + "," + str(self.positions[i])
-            i += 1
-        return stats
-
 
 def readDocIds(docids_file):
     file_pointer = open(docids_file, "r", encoding="utf8", errors='ignore')
     file_data = file_pointer.read()
     file_lines = file_data.split('\n')
-    documents = []
+    documents = dict()
     for line in file_lines:
         if len(line) > 1:
             arr = line.split('\t')
-            documents.append(Doc(int(arr[0]), arr[1]))
+            documents[arr[1]] = int(arr[0]) # <DOC_NAME, DOC_ID>
     return documents
 
 
@@ -57,11 +20,11 @@ def readTermIds(termids_file):
     file_pointer = open(termids_file, "r", encoding="utf8", errors='ignore')
     file_data = file_pointer.read()
     file_lines = file_data.split('\n')
-    terms = []
+    terms = dict()
     for line in file_lines:
         if len(line) > 1:
             arr = line.split('\t')
-            terms.append(Term(int(arr[0]), arr[1]))
+            terms[arr[1]] = int(arr[0]) # <TERM, TERM_ID>
     return terms
 
 
@@ -104,16 +67,9 @@ def stemming(words):
     return words
 
 
-def saveDocInTerm(term_value, terms_array, doc_id, doc_pos):
-    # linear search to find the term id.
-    for term in terms_array:
-        if term_value == term.value:
-            term.add(doc_id, doc_pos)
-
-
-def deltaEncoding(terms_array):
-    for term in terms_array:
-        if len(term.docids) < 1:
+def deltaEncoding(terms_dict):
+    for term in terms_dict:
+        if len() < 1:
             continue
         i = 1
         last_docid = term.docids[0]
@@ -141,24 +97,50 @@ def invertedIndex():
     documents = readDocIds(config.DOCID_FILE)
     terms = readTermIds(config.TERMID_FILE)
     count = 1
+
+    # I'm going to save like index = <term_id, <doc_id, positons[]>>
+
+    index = dict()
+    for val in terms:
+        index[terms[val]] = dict() # index = <term_id, <>>
+
     for doc in documents:
-        file_pointer = open(config.CORPUS_DIR + doc.value, "r", encoding="utf8", errors='ignore')
+        file_pointer = open(config.CORPUS_DIR + doc, "r", encoding="utf8", errors='ignore')
         file_html = file_pointer.read()
         file_text = parseHtml(file_html)
         tokens = tokenize(file_text)
         tokens = stopwording(tokens)
         words = stemming(tokens)
         position_count = 1
+        doc_index = dict()
+        doc_id = documents[doc]
         for word in words:
-            saveDocInTerm(word, terms, doc.id, position_count)
+            term_id = terms[word]
+            try:
+                index[term_id][doc_id].append(position_count)
+            except:
+                index[term_id][doc_id] = []
+                index[term_id][doc_id].append(position_count)
+
             position_count += 1
         count += 1
         if count > 10:
             break
         print("files done: " + str(count-1))
 
-    deltaEncoding(terms)
-    saveIndex(terms)
+    for ind in index:
+        if len(index[ind]) > 0:
+            total_occ = 0
+            output = ""
+            for doc in index[ind]:
+                for pos in index[ind][doc]:
+                    output += " " + str(doc) + "," + str(pos)
+                    total_occ+=1
+
+            print(str(ind) + " " + str(total_occ) + " " + str(len(index[ind])) + output)
+
+    #deltaEncoding(index, terms, documents)
+    # saveIndex(terms)
     # for term in terms:
     #     #     if len(term.docids) > 0:
     #     #         print(term.getStats())
